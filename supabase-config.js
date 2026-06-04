d9c368a5adb5979e6255d3462dc0e0b25c5d1638
Salahotdinov <92904463+Salahotdinov@users.noreply.github.com>
2026-06-04 20:36:21 +0500

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// Замените на ваши реальные данные из панели Supabase
+const SUPABASE_URL = 'https://qiqbkwhqxfapiqskycch.supabase.co/rest';
+const SUPABASE_KEY = 'sb_publishable_1tk4TlQdwdEU-l9clvyduQ_mCCTwXGd'; 
+
+// Инициализация клиента Supabase
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
